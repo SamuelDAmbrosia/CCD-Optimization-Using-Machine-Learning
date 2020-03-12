04b0f9ab7b82f13b87a54748d555e677991799df
Sam D'Ambrosia <Sam.dambrosia@gmail.com>
2020-03-12 16:20:08 -0700

    Cleaned up Graphing.py

--- Graphing.py
+++ Graphing.py
@@ -14,20 +14,14 @@ def graph1D(imgcsv, setting, output, smin=-inf, smax=inf, omin=-inf, omax=inf):
     Generates a scatter plot showing relationship between one setting var and one output var
     '''
     
-    imgdat = pd.read_csv(imgcsv)
-    
-    sets = []
-    outs = []
-    
-    for i in range(len(imgdat)):
-        if((smin < float(imgdat[setting][i]) < smax) and (omin < float(i) < omax)):
-            sets.append(float(imgdat[setting][i]))
-            outs.append(float(imgdat[output][i]))
+    #apply constraints to data set
+    imgdat = pd.read_csv(imgcsv).query('('+str(smin)+' < '+str(setting)+' < '+str(smax)+') and ('+str(omin)+' < '+str(output)+' < '+str(omax)+')')
     
     colors = 'blue'
     area = 7
 
-    plt.scatter(sets, outs, s=area, c=colors, alpha=0.5)
+    #create graph
+    plt.scatter(imgdat[setting], imgdat[output], s=area, c=colors, alpha=0.5)
     plt.title(setting + " to " + output)
     plt.xlabel(setting)
     plt.ylabel(output)
@@ -35,6 +29,6 @@ def graph1D(imgcsv, setting, output, smin=-inf, smax=inf, omin=-inf, omax=inf):
     
 def main(args):
     graph1D(args[1], args[2], int(args[3]), int(args[4]), args[5], int(args[6]), int(args[7]))
-    
-if(len(args) == 8):
+
+if(args[0] == 'Graphing.py'):
     main(args)
